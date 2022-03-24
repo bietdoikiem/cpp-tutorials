efba99fc706492bbf1390eb95a605c4e7e506972
bietdoikiem <minhlucky2408@gmail.com>
2022-03-24 18:55:07 +0700

    feat+refactor: add comment module+edit build task

---
+++ comment/main.cpp
@@ -0,0 +1,12 @@
+// Import iostream library
+#include <iostream>
+
+/*
+  This is a block comment
+*/
+
+int main() {
+  // This is going to print the "Hello World!" string to the console
+  std::cout << "Hello World!" << std::endl;
+  return 0; // Ends the program
+}
