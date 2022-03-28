f2eebc47deb911b230d70573363dbd430406a8c1
bietdoikiem <minhlucky2408@gmail.com>
2022-03-28 17:35:13 +0700

    feat: add string declaration

---
+++ declare-strings/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  std::string fullName;  // Empty string
+  std::string planet = "Earth";
+  std::string car{"Mercedes"};
+  std::cout << "fullName: " << fullName << std::endl;
+  std::cout << "planet: " << planet << std::endl;
+  std::cout << "car: " << car << std::endl;
+  return 0;
+}
